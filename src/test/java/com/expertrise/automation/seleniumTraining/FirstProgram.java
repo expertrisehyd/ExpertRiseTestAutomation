0be23ef82a042738b2d65c969ed62a102544569f
ExpertRise <org@expertrise.com>
2026-06-15 09:08:44 +0530

    added basic program

---
+++ src/test/java/com/expertrise/automation/seleniumTraining/FirstProgram.java
@@ -0,0 +1,21 @@
+package com.expertrise.automation.seleniumTraining;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstProgram {
+   // public WebDriver driver;
+    public  static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver  driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com");
+        System.out.println("Title : " + driver.getTitle());
+        System.out.println("URL   : " + driver.getCurrentUrl());
+        driver.quit();
+    }
+
+
+}
