e2da29a28812a62ab585106aefb5ed6b821c5980
Kasuk <kasuk@example.com>
2026-06-18 13:44:28 +0530

    Login

---
+++ src/test/java/com/expertrise/automation/seleniumTraining/webdrivercore/Login.java
@@ -0,0 +1,110 @@
+package com.expertrise.automation.seleniumTraining.webdrivercore;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.math3.analysis.function.Exp;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
+
+public class Login {
+    static WebDriver driver;
+    static WebDriverWait wait;
+
+
+    @BeforeAll
+    public static void BrowserConfig(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com");
+
+    }
+    @Test
+    public void loginTest() throws InterruptedException {
+        BrowserNavigation();
+        userName();
+        password();
+        clickButton();
+        verifyDashBoard();
+        clickProfile();
+        logOut();
+
+
+    }
+
+    public void BrowserNavigation() throws InterruptedException {
+        driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("username")));
+        System.out.println("Navigated to Login page");
+
+    }
+
+    public void userName(){
+        driver.findElement(By.name("username")).sendKeys("Admin");
+
+    }
+
+    public void password(){
+        driver.findElement(By.name("password")).sendKeys("admin123");
+    }
+
+    public void clickButton() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")).click();
+        wait.until(
+                ExpectedConditions.urlContains("dashboard"));
+        System.out.println(driver.getCurrentUrl());
+
+    }
+
+    public void verifyDashBoard(){
+        WebElement dashboard = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//h6")));
+        System.out.println(dashboard.getText());
+
+        boolean status = dashboard.isDisplayed();
+        System.out.println("Logged in ?? : "+ status);
+    }
+
+
+    public void clickProfile(){
+        WebElement profile = wait.until(ExpectedConditions.elementToBeClickable(By.className("oxd-topbar-header-userarea")));
+        profile.click();
+        System.out.println("Profile clicked");
+        List<WebElement> menuItems = wait.until(
+                ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                        By.className("oxd-dropdown-menu")));
+
+        for(WebElement item : menuItems){
+            System.out.println(item.getText());
+        }
+
+    }
+
+    public void logOut(){
+        driver.findElement(
+                        By.xpath("//a[text()='Logout']"))
+                .click();
+        System.out.println("Logged out");
+    }
+
+    @AfterAll
+    static void tearDown() {
+
+    }
+
+}
---
+++ target/test-classes/features/Test.feature
@@ -0,0 +1,11 @@
+Feature:
+    This is a placeholder feature
+    As a tester
+    I want to have a placeholder test
+    So that I can demonstrate the structure of a feature file
+
+    @placeholder
+  Scenario:  This is a placeholder test
+    Given This is a placeholder
+    When  I run the test
+    Then  It should pass successfully
