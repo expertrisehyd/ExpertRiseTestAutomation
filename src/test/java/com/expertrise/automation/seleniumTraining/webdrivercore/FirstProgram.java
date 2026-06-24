aab865454379c5a89a04322fb558070d7da28c8e
ExpertRise <org@expertrise.com>
2026-06-24 10:01:58 +0530

    added scroll  Test and Slider  test scripts Modified Fire Fox ,first Program , screen shot

--- src/test/java/com/expertrise/automation/seleniumTraining/webdrivercore/FirstProgram.java
+++ src/test/java/com/expertrise/automation/seleniumTraining/webdrivercore/FirstProgram.java
@@ -3,10 +3,13 @@ package com.expertrise.automation.seleniumTraining.webdrivercore;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class FirstProgram {
 
-    public  static void main(String[] args) {
+
+    @Test(groups = { "browser" })
+    public void LaunchBrowser(){
         WebDriverManager.chromedriver().setup();
         WebDriver  driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -15,6 +18,4 @@ public class FirstProgram {
         System.out.println("URL   : " + driver.getCurrentUrl());
         driver.quit();
     }
-
-
 }
